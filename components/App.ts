e4b3c5c9bea66471432eb487084249d14eea3933
DmDespert <dimitri.despert@gmail.com>
2021-08-12 08:55:28 +0200

    feat: generate html in objects

--- components/App.ts
+++ components/App.ts
@@ -4,12 +4,14 @@ import * as ε from '../tools/ε/';
 import Header from './Header';
 
 class App extends ε.Component {
-// We want to render html like: 
 
-  //     <div>
-  //       <Header />
-  //     </div>
-  render() {}
+  render() {
+    return ε.createElement(
+      'div',
+      {},
+      [ε.createElement(Header, {}, [])]
+      );
+  }
 }
 
 export default App;
--- tools/ε/index.ts
+++ tools/ε/index.ts
@@ -5,13 +5,54 @@ abstract class Component {
   abstract render(): Element
 }
 
+function createElement(name, attributes, children) {
+  return {
+    name,
+    attributes,
+    children
+  };
+};
+
+function elementsToHTML(virtualDom) {
+  // TODO
+}
+
+function getElement(element) {
+  let elementToReturn;
+  if (typeof element === 'string') {
+    elementToReturn = element;
+  } else if(typeof element.name === 'string') {
+    const childrenCol = [];
+    element.children.forEach(e => {
+      childrenCol.push(getElement(e));
+    });
+    element.children = childrenCol;
+    elementToReturn = element;
+  } else {
+    const instance = new element.name();
+    const render = instance.render();
+    elementToReturn = getElement(render);
+  }
+
+  return elementToReturn;
+}
+
+
+
+function getVirtualDom(element) {
+  return getElement(element);
+}
+
 function start(rootComponent, rootHtml: HTMLElement): void {
   const rootInstance = new rootComponent();
   const rootRender = rootInstance.render();
-  console.log(rootInstance);
+  const virtualDom = getVirtualDom(rootRender);
+  const site = elementsToHTML(virtualDom);
+  console.log(site);
 }
 
 export {
   Component,
-  start
+  start,
+  createElement
 };
